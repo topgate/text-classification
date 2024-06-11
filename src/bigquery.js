8247fd72e22c51a62ae7f320d7d272e119d302a8
r-sakamoto <99632267+r-sakamoto-tg@users.noreply.github.com>
2024-06-11 19:36:18 +0900

    feat: BQ にレコードを挿入する

--- src/bigquery.js
+++ src/bigquery.js
@@ -5,4 +5,5 @@ const dataset = process.env.DATASET_ID;
 const table = process.env.TABLE_ID;
 
 export async function insertBQ(fileName, category, createdAt) {
+    return bigquery.dataset(dataset).table(table).insert({ file_name: fileName, category: category, createdAt: createdAt });
 }
